a76fca23dc1ff8fc986a89deedf566b6c6d25318
loubarb <louis.barbati@gmail.com>
2022-04-02 23:39:41 -0400

    Create function to access and capture user's webcam device

--- scripts.js
+++ scripts.js
@@ -3,3 +3,22 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// navigator.mediaDevices.getUserMedia grabs user's camera source & asks permission to use it
+// returns promise; call .then with localMediaStream
+// set video source to be localMediaStream
+  // window.URL.createObjectURL converts mediaStream into format to work with video player
+// video.play captures continously to play it
+// .catch is if user denies webcam access
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
+getVideo();
