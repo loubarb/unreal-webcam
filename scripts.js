14d647a8fd7495ba78c9be4b182bb5077d894284
loubarb <louis.barbati@gmail.com>
2022-04-04 01:21:30 -0400

    Edited video function to update to newer syntax

--- scripts.js
+++ scripts.js
@@ -5,23 +5,18 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 // navigator.mediaDevices.getUserMedia grabs user's camera source & asks permission to use it
-// returns promise; call .then with localMediaStream
-// set video source to be localMediaStream
-  // window.URL.createObjectURL converts mediaStream into format to work with video player
+// returns promise; call .then
+// set video source to be mediaStream
 // video.play captures continously to play it
 // .catch is if user denies webcam access
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
-      // console.log(localMediaStream);
-      video.src = window.URL.createObjectURL(localMediaStream);
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(mediaStream => {
+      video.srcObject = mediaStream;
       video.play();
     })
-    .catch(err => {
-      console.error(err);
-    })
+    .catch(err);
 }
-
 
 // get width & height of video
 // make canvas same width & height of video
